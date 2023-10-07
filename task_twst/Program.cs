f16aa15019c9349de58faebb71efdbdc3c5561d8
danger4k <danger4k@gmail.com>
2023-10-07 11:52:46 +0300

    добавил gitignore

---
+++ task_twst/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+public class Answer
+{
+    static void PrintEvenNumbers(int number)
+    {
+        // Введите свое решение ниже
+
+
+
+        for (int i = 1; i <= number; i++)
+        {
+            if (i % 2 == 0)
+            {
+                System.Console.Write(i + "  ");
+            }
+
+        }
+
+
+
+
+
+
+    }
+
+
+    // Не удаляйте и не меняйте метод Main! 
+    static public void Main(string[] args)
+    {
+        int number;
+
+        if (args.Length >= 1)
+        {
+            number = int.Parse(args[0]);
+        }
+        else
+        {
+            // Здесь вы можете поменять значения для отправки кода на Выполнение
+            number = 7;
+        }
+
+        // Не удаляйте строки ниже
+        PrintEvenNumbers(number);
+    }
+}
